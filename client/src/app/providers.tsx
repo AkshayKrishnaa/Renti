fa4b949b18179dbe774feba396df87a95b19560a
AkshayKrishnaa <akshaydkrishna12@gmail.com>
2025-11-26 00:00:02 -0700

    add basic sign in page

--- client/src/app/providers.tsx
+++ client/src/app/providers.tsx
@@ -1,9 +1,27 @@
 "use client";
 
 import StoreProvider from "@/state/redux";
+import { Authenticator } from "@aws-amplify/ui-react";
+import { usePathname } from "next/navigation";
+import Auth from "./(auth)/authProvider";
+
+const PUBLIC_ROUTES = ["/", "/landing"];
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
-    return <StoreProvider>{children}</StoreProvider>;
+  const pathname = usePathname();
+  const isPublicRoute = PUBLIC_ROUTES.includes(pathname ?? "");
+
+  return (
+    <StoreProvider>
+      {isPublicRoute ? (
+        children
+      ) : (
+        <Authenticator.Provider>
+          <Auth>{children}</Auth>
+        </Authenticator.Provider>
+      )}
+    </StoreProvider>
+  );
 };
 
 export default Providers;
